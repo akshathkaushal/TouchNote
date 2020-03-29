6470618f8f800b7df2ab104a0870879737341559
akshathkaushal <akshath.kaushal@iiitb.org>
2020-03-29 20:35:28 +0530

    Updated the UI

--- app/TodoList/models.py
+++ app/TodoList/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class List(models.Model):
-    item = models.CharField(max_length=200)
+    item = models.CharField(max_length=500)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
